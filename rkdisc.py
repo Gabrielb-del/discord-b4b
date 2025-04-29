972e5fba333e4d6104e093f768e9a298e171a3d5
Gabriel Baunilia <gabrielbaunilia123@gmail.com>
2025-04-29 16:14:04 -0300

    Adicionando origem SMS

--- rkdisc.py
+++ rkdisc.py
@@ -112,7 +112,7 @@ MAPEAMENTO_REVERSO = {
 operadores = [
             "Abigail", "Alyssa", "Giovana Vitória", "João", "Mia",
             "Rita", "Sara", "Thaleco", "Vinícius", "Yasmin", "Yuri", "Gabriela", "Beatriz Duarte",
-            "Maria Luisa", "Isaac", "Beatriz Oliveira", "Giovana Martins", "Sofia", "Matheus", "Christyan", 
+            "Maria Luisa", "Isaac", "Beatriz Oliveira", "Giovana Martins", "Sofia", "Christyan", 
             "Mariana", "Tamiris", "Juliana", "Larissa", "Beatriz Soares", "Victor Hugo", "Alexandre"
         ]
 
